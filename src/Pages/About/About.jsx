819acf790cf49dba279892f5e6f9dc95702e167b
Ahadu Chere <ahaduchere546@gmail.com>
2024-11-27 01:31:43 -0500

    About Page Banner Added

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,10 +1,13 @@
 import Header from '../../Components/Header/Header';
+import Banner from '../../Components/Banner/Banner';
 function About() {
   return (
     <div>
       <Header />
+      <Banner />
     </div>
   )
 }
+
 
 export default About
